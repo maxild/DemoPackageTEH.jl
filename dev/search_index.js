df73632d8723395dc886c34f37eeeb88135155dc
Documenter.jl <documenter@juliadocs.github.io>
2022-01-22 16:55:40 +0000

    build based on 90af6c5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageTEH","category":"page"},{"location":"#DemoPackageTEH","page":"Home","title":"DemoPackageTEH","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageTEH.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageTEH]","category":"page"}]
+}
